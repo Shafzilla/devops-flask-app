7cc2e3de6c5fb7e1a836a9bf1c25c837e0fa9722
mohammad mujahed <mohommadshafi22@gmail.com>
2025-09-29 20:57:58 +0000

    change case

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ def about():
 	return '<p>This application is running on the Flask web framework. To learn more about this, go to</p><a href="https://flask.palletsprojects.com/en/stable/">Flask</a>'
 
 def contact():
-	return '<p>c23502209@mytudublin.ie</p>'
+	return '<p>C23502209@mytudublin.ie</p>'
 
 def form():
 	return '<p>fill the form</p>'
