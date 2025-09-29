7ae547304654e0445910392f7cd988aa9b8c5835
mohammad mujahed <mohommadshafi22@gmail.com>
2025-09-29 20:34:30 +0000

    updated hello.py

--- hello.py
+++ hello.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<a href="http://localhost:5000/about">about page</a>'
+	return '<a href="http://localhost:5000/about">about page</a><br><a href="http://localhost:5000/contact"><Contact page</a>'
 
 @app.route('/about')
 def about():
 	return '<p>This application is running on the Flask web framework. To learn more about this, go to</p><a href="https://flask.palletsprojects.com/en/stable/">Flask</a>'
+
+def contact():
+	return '<p>c23502209@mytudublin.ie</p>'
